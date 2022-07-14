39a44e225d35c751f853f973db6dc5aa8e2b3639
safaziedi <safaziedi070@gmail.com>
2022-07-14 11:19:58 +0100

    integrating with the MongoDB database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://dbUser:safaziadi07012001@cluster0.thbgv.mongodb.net/forum?retryWrites=true&w=majority'),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
